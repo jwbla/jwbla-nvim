5081e91dc8af42bab401cdc348e13dabc0df5ab9
jwbla <jwbla@users.noreply.github.com>
2024-09-19 18:54:02 -0400

    update small config stuff

--- lua/jwbla/plugins/lsp.lua
+++ lua/jwbla/plugins/lsp.lua
@@ -27,6 +27,7 @@ return {
       require("mason-lspconfig").setup({
           ensure_installed = {
               "lua_ls",
+              "clangd"
           },
           handlers = {
               function(server_name) -- default handler (optional)
