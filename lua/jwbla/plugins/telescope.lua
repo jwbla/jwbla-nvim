db39b4b262126ac994d0038d4890548e6f94bed0
jess <16634555+jwbla@users.noreply.github.com>
2025-02-02 22:51:40 +0000

    add on to telescope keybinds

--- lua/jwbla/plugins/telescope.lua
+++ lua/jwbla/plugins/telescope.lua
@@ -10,6 +10,8 @@ return {
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {});
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {});
             builtin.grep_string({search = vim.fn.input("Grep > ") });
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {});
