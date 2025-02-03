4486cd41ef46c91dabdaf96855bc1124299643f1
jwbla <jwbla@users.noreply.github.com>
2025-02-03 17:03:44 -0500

    add in ctrl+/ for comment

--- lua/jwbla/plugins/comment.lua
+++ lua/jwbla/plugins/comment.lua
@@ -1,6 +1,21 @@
 return {
     'numToStr/Comment.nvim',
     opts = {
-        -- add options here
-     }
+        -- Enable default mappings
+        mappings = {
+            basic = true,
+            extra = true,
+        },
+    },
+    config = function(_, opts)
+        require("Comment").setup(opts)
+
+        -- Ctrl + / in Normal mode
+        vim.keymap.set("n", "<C-_>", function()
+            require("Comment.api").toggle.linewise.current()
+        end, { desc = "Toggle comment for current line" })
+
+        -- Ctrl + / in Visual mode
+        vim.keymap.set("v", "<C-_>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = "Toggle comment for selection" })
+    end
 }
