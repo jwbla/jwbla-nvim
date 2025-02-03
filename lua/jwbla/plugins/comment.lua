8c741c6eadd78f01735e6fd96b507e854f163ad3
jwbla <jwbla@users.noreply.github.com>
2025-02-03 18:48:49 -0500

    fix comment to work on win/linux

--- lua/jwbla/plugins/comment.lua
+++ lua/jwbla/plugins/comment.lua
@@ -1,21 +1,21 @@
 return {
     'numToStr/Comment.nvim',
-    opts = {
-        -- Enable default mappings
-        mappings = {
-            basic = true,
-            extra = true,
-        },
-    },
+    opts = {},
     config = function(_, opts)
         require("Comment").setup(opts)
 
-        -- Ctrl + / in Normal mode
+        -- Normal mode: Toggle comment
         vim.keymap.set("n", "<C-_>", function()
             require("Comment.api").toggle.linewise.current()
         end, { desc = "Toggle comment for current line" })
 
-        -- Ctrl + / in Visual mode
+        vim.keymap.set("n", "<C-/>", function()
+            require("Comment.api").toggle.linewise.current()
+        end, { desc = "Toggle comment for current line (Linux)" })
+
+        -- Visual mode: Toggle comment
         vim.keymap.set("v", "<C-_>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = "Toggle comment for selection" })
+
+        vim.keymap.set("v", "<C-/>", "<ESC><CMD>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = "Toggle comment for selection (Linux)" })
     end
 }
