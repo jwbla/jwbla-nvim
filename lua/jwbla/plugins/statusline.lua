e3e6033568803a4b5826252514662966539f72ae
jwbla <jwbla@users.noreply.github.com>
2025-07-10 16:41:52 -0400

    move statusbar back to normal place (teehee); update pkgs

--- lua/jwbla/plugins/statusline.lua
+++ lua/jwbla/plugins/statusline.lua
@@ -22,12 +22,12 @@ return {
                 }
             },
             sections = {
-                -- lualine_a = {'mode'},
-                -- lualine_b = {'branch', 'diff', 'diagnostics'},
-                -- lualine_c = {'filename'},
-                -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-                -- lualine_y = {'progress'},
-                -- lualine_z = {'location'}
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
             },
             inactive_sections = {
                 lualine_a = {},
@@ -38,12 +38,12 @@ return {
                 lualine_z = {}
             },
             tabline = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
+                -- lualine_a = {'mode'},
+                -- lualine_b = {'branch', 'diff', 'diagnostics'},
+                -- lualine_c = {'filename'},
+                -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+                -- lualine_y = {'progress'},
+                -- lualine_z = {'location'}
             },
             winbar = {},
             inactive_winbar = {},
