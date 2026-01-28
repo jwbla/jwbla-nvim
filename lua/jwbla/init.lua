766d76e80a91b9fb29a9168713c0c839669c4f8d
jess <16634555+jwbla@users.noreply.github.com>
2026-01-28 20:47:40 +0000

    fix foldlevel; specify main branch for treesitter; specify install site for parsers

--- lua/jwbla/init.lua
+++ lua/jwbla/init.lua
@@ -8,6 +8,7 @@ local autocmd = vim.api.nvim_create_autocmd
 vim.cmd.colorscheme "catppuccin-mocha"
 vim.opt.laststatus=3
 vim.opt.foldmethod="indent"
+vim.opt.foldlevel=99
 
 autocmd('LspAttach', {
     callback = function(e)
--- lua/jwbla/treesitter.lua
+++ lua/jwbla/treesitter.lua
@@ -1,3 +1,7 @@
+require('nvim-treesitter').setup({
+    install_dir = vim.fn.stdpath('data') .. '/site'
+})
+
 require('nvim-treesitter').install({
     "bash",
     "c",
